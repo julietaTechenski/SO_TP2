3b08df45f529596078c20b7748dea4fa5f4a7037
Julieta Techenski <119064935+julietaTechenski@users.noreply.github.com>
2024-05-15 12:44:44 -0300

    added MemoryManagerCDT struct

--- Kernel/buddyAlloc.c
+++ Kernel/buddyAlloc.c
@@ -1,11 +1,8 @@
-//
-// Created by Usuario on 15/5/2024.
-//
+#include "memory_manager.h"
 
-#include "include/memory_manager.h"
-
+// recibo endOfModules YA ALINEADO
 void buddy_init(uint8_t * ptr) {
-    // Inicialización específica del gestor de memoria Buddy
+
 }
 
 void* buddy_alloc(size_t size) {
--- Kernel/include/memory_manager.h
+++ Kernel/include/memory_manager.h
@@ -11,4 +11,9 @@ void memory_manager_init(uint8_t * ptr);
 void* memory_manager_alloc(size_t size);
 void memory_manager_free(void* ptr);
 
+
+typedef struct MemoryManagerCDT {
+    uint8_t *nextAddress;
+} MemoryManagerCDT;
+
 #endif // MEMORY_MANAGER_H
