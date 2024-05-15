deb821480c3347bd7f944f09bd9a65e6f7d842ff
Lucas Campoli <lcampoli@itba.edu.ar>
2024-05-15 12:16:20 -0300

    allocator files

--- Kernel/freeList.c
+++ Kernel/freeList.c
@@ -2,5 +2,16 @@
 // Created by Usuario on 15/5/2024.
 //
 
-#include "include/freeList.h"
+#include "memory_manager.h"
 
+void slab_init() {
+    // Inicialización específica del gestor de memoria Slab
+}
+
+void* slab_alloc(size_t size) {
+    // Lógica de asignación para el gestor Slab
+}
+
+void slab_free(void* ptr) {
+    // Lógica de liberación para el gestor Slab
+}
