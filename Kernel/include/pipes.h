6bff86e1060ec0c7127c9e2a52164e6d76c8d1e7
Julieta Techenski <119064935+julietaTechenski@users.noreply.github.com>
2024-06-05 19:42:53 -0300

    pipes change in parameter and pipe implementation

--- Kernel/include/pipes.h
+++ Kernel/include/pipes.h
@@ -1,6 +1,8 @@
 #ifndef SO_TP2_PIPES_H
 #define SO_TP2_PIPES_H
 
+#include "memory_manager.h"
+
 typedef struct PCB {
     char name[MAX_NAME_LENGTH];
     uint64_t pid;
@@ -14,5 +16,8 @@ typedef struct PCB {
     struct PCB *next;
 
 } PCB;
+
+int pipe(void * pipesfd[2]);
+int dup(void* p, int oldfd, void* pipedir);
 
 #endif //SO_TP2_PIPES_H
