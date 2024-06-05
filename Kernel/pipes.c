3ac9ed64422f70bca223f445c7951b6d88728620
Julieta Techenski <119064935+julietaTechenski@users.noreply.github.com>
2024-06-05 19:53:24 -0300

    implementation

--- Kernel/pipes.c
+++ Kernel/pipes.c
@@ -1,8 +1,5 @@
 #include "include/pipes.h"
 
-
-
-
 int pipe(void * pipesfd[2]){
     pipesfd[0] = pipesfd[1] = mm_alloc(sizeof(char)*128);
     if(pipesfd[0] == NULL){
@@ -15,4 +12,5 @@ int dup(int oldfd, void* pipedir){
     uint64_t ppid = getpid();
     PCB *p = getprocess(ppid);
     p->fds[oldfd] = pipedir;
+    return 1;
 }
