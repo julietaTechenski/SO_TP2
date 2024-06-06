09e926faade36316b78393e862841854370bfede
Julieta Techenski <119064935+julietaTechenski@users.noreply.github.com>
2024-06-06 20:44:58 -0300

    mem w/ buddy

--- Kernel/buddyAlloc.c
+++ Kernel/buddyAlloc.c
@@ -1,75 +1,129 @@
+#include "../include/memory_manager.h"
+
 #ifdef BUDDY
-#include "./include/memory_manager.h"
+
 #define PAGE_SIZE 4096 // (4KB)
-#define MAX_ORDER 5
-#define sizes (MAX_ORDER + 1)
 
 typedef struct block {
     struct block *next;
     size_t size;
 }block_t;
 
-static block_t* free_blocks[sizes];
+static block_t** free_blocks;
+static uint8_t *base_memory;
+static size_t max_memory_size;
+static int max_order;
+
+int freeMem;
+int total;
+
+int firstAllocRun = 1;
+int firstFreeRun = 1;
+
+
 
 /* blocks in freelists[i] are of size 2**(i + 2) * PAGE_SIZE */
 #define BLOCKSIZE(i) ((size_t)(1 << (i + 2)) * PAGE_SIZE)
 
-/* the address of the buddy of a block from freelists[i]. */
+/* the address of the buddy of a block from freelists[i] */
 #define BUDDYOF(b, i) ((block_t*)((uintptr_t)(b) ^ (BLOCKSIZE(i))))
 
-void error(const char *msg) {
-    printf("Error: %s\n", msg);
+
+int log2(size_t n) {
+    int log = 0;
+    while (n >>= 1)
+        log++;
+    return log;
 }
 
-void mm_init(uint8_t * ptr, size_t max_size) {
-    for(int i = 0; i < sizes; i++)
-        free_blocks[i] = NULL;
+size_t pow2(int n) {
+    return (size_t)1 << n;
+}
 
-    free_blocks[MAX_ORDER] = (block_t *)ptr;
-    free_blocks[MAX_ORDER]->next = NULL;
-    initial_block->size = BLOCKSIZE(MAX_ORDER);
+
+void mm_init(void * ptr, size_t max_size) {
+    base_memory = (uint8_t*) ptr;
+    freeMem = total = max_memory_size = max_size;
+
+    max_order = log2(max_size/PAGE_SIZE) - 2;
+
+    size_t free_blocks_size = (max_order + 1) * sizeof(block_t*);
+    free_blocks = (block_t**)base_memory;
+    base_memory += free_blocks_size;
+
+    uintptr_t addr = (uintptr_t)base_memory;
+    addr = (addr + PAGE_SIZE - 1) & ~(PAGE_SIZE - 1);
+    base_memory = (uint8_t *)addr;
+
+    for(int i= 0; i <= max_order; i++){
+        free_blocks[i] = NULL;
+    }
+
+    int initial_order = max_order;
+    size_t block_size = BLOCKSIZE(initial_order);
+
+    block_t * initial_block = (block_t *)base_memory;
+    free_blocks[initial_order] = initial_block;
+    initial_block->next = NULL;
+    initial_block->size = block_size;
 }
 
 void* mm_alloc(size_t size) {
+    if(firstAllocRun){
+        freeMem -= sizeof(block_t) + size;
+        firstAllocRun = !firstAllocRun;
+    }
     int i = 0;
 
-    while(BLOCKSIZE(i) < size){
+    while(BLOCKSIZE(i) < size + sizeof(block_t) && i <= max_order){
         i++;
     }
 
-    if (i >= sizes) {
-        error("no space available");
+    if(i > max_order){
         return NULL;
-    }
-    else if (free_blocks[i] != NULL) {
-        // se encotro un bloque del tamaño que necesitamos
+    } else if (free_blocks[i] != NULL) {
         block_t* block = free_blocks[i];
         free_blocks[i] = free_blocks[i]->next;
         block->size = BLOCKSIZE(i);
-        return block;
-    }
-    else {
-        block_t* block = allocate(BLOCKSIZE(i + 1));  //llamada recursiva para obtener un bloque mas grande
+        return (void*)(block);
+    } else {
+        block_t* block = (block_t*)mm_alloc(BLOCKSIZE(i + 1) - sizeof(block_t));
         if (block != NULL) {
-            // Dividimos el bloque y ponemos el buddy en la lista de bloques libres
+            block = (block_t*)((uintptr_t)block - (uintptr_t)base_memory); // reative position
+
             block_t* buddy = BUDDYOF(block, i);
+            buddy = (block_t*)((uintptr_t)buddy + (uintptr_t)base_memory); // reverse to absolute after getting the buddy
+
             buddy->size = BLOCKSIZE(i);
             buddy->next = free_blocks[i];
+
             free_blocks[i] = buddy;
+            block = (block_t*)((uintptr_t)block + (uintptr_t)base_memory);  // reverse to absolute after getting the buddy
             block->size = BLOCKSIZE(i);
+            return (void*)(block);
         }
-        return block;
+        return NULL;
     }
 }
 
 void mm_free(void * ptr) {
-    size_t size = (block_t*)block->size;
+     if(firstFreeRun){
+        freeMem += sizeof(block_t) + ((block_t*)(ptr-sizeof(block_t)))->size;
+        firstAllocRun = !firstAllocRun;
+    }
+    block_t *block = (block_t *)ptr - sizeof(block_t) ;
     int i = 0;
-
-    while(BLOCKSIZE(i) < size){
+    while(BLOCKSIZE(i) < block->size && i <= max_order){
         i++;
     }
+
+    if(i > max_order){
+        return;
+    }
+    block = (block_t*)((uintptr_t)block - (uintptr_t)base_memory);
     block_t* buddy = BUDDYOF(block, i);
+    block = (block_t*)((uintptr_t)block + (uintptr_t)base_memory);
+    buddy = (block_t*)((uintptr_t)buddy + (uintptr_t)base_memory);
     block_t** p = &free_blocks[i];
 
     //verifico que no se encuetre en la lisat de bloques libres
@@ -81,14 +135,38 @@ void mm_free(void * ptr) {
         block->next = free_blocks[i];
         free_blocks[i] = block;
     }else {
-        // el buddy se encuntra ocupado, hay q librearlo
+        // el buddy se encuntra libre, fusionamos los bloques
         *p = buddy->next;
 
-        if(block > buddy)
-            deallocate(buddy, BLOCKSIZE(i+1));
-        else
-            deallocate(block, BLOCKSIZE(i+1));
+        if(block > buddy){
+            mm_free(buddy + sizeof(block_t));
+        }else{
+            mm_free(block + sizeof(block_t));
+        }
     }
+}
+
+
+void mm_state(){
+    char s[20];
+    int longitud;
+
+    longitud = intToString(total, s);
+    writeString(1, "Memoria total: ", 15);
+    writeString(1, s, longitud);
+    writeString(1, "\n", 1);
+
+
+    longitud = intToString(freeMem, s);
+    writeString(1, "Memoria libre: ", 15);
+    writeString(1, s, longitud);
+    writeString(1, "\n", 1);
+
+    longitud = intToString(total - freeMem, s);
+    writeString(1, "Memoria ocupada: ", 17);
+    writeString(1, s, longitud);
+    writeString(1, "\n", 1);
+
 }
 
 #endif
