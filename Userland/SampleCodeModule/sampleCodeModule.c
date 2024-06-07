8329f8eb7908ed78d113f2b342d7d35a299f851f
LucasCampoli <campolilucas@gmail.com>
2024-06-07 17:17:52 -0300

    scheduler

--- Userland/SampleCodeModule/sampleCodeModule.c
+++ Userland/SampleCodeModule/sampleCodeModule.c
@@ -1,7 +1,12 @@
 #include "include/sampleCodeModule.h"
 
+void test(){
+    printf("test process creation");
+}
+
 int main() {
     char *argvAux[] = {0};
     my_createProcess(&shell, "shell", 0, argvAux);
+    my_createProcess(&test, "test", 0, argvAux);
     return 0;
 }
