32e04f2c23a38750e91a887e904a303d5b3e2eed
Julieta Techenski <119064935+julietaTechenski@users.noreply.github.com>
2024-06-09 18:20:12 -0300

    moved process.c

--- compile.sh
+++ compile.sh
@@ -2,4 +2,4 @@ docker start TPEFINAL
 docker exec -ti TPEFINAL make clean -C/root/Toolchain
 docker exec -ti TPEFINAL make clean -C/root/
 docker exec -ti TPEFINAL make -C/root/Toolchain
-docker exec -ti TPEFINAL make -C/root
+docker exec -ti TPEFINAL make -C/root/
