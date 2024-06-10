e5081e6f01ccaf095ee75d0bd50d5318318e31ec
Julieta Techenski <119064935+julietaTechenski@users.noreply.github.com>
2024-06-10 01:46:32 -0300

    ./compile.sh LIST o default buddy

--- compile.sh
+++ compile.sh
@@ -1,5 +1,11 @@
+if [ "$1" == "LIST" ]; then
+  MEMORY_MANAGER="LIST"
+else
+  MEMORY_MANAGER="BUDDY"
+fi
+
 docker start TPEFINAL
 docker exec -ti TPEFINAL make clean -C/root/Toolchain
 docker exec -ti TPEFINAL make clean -C/root/
 docker exec -ti TPEFINAL make -C/root/Toolchain
-docker exec -ti TPEFINAL make -C/root/
+docker exec -ti TPEFINAL make -C/root/ MEMORY_MANAGER=$MEMORY_MANAGER
