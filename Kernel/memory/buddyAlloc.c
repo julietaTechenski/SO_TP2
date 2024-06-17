cc3d5a862c7d8ac75df0ecbae4add8cd8d200e30
Julieta Techenski <119064935+julietaTechenski@users.noreply.github.com>
2024-06-17 20:12:38 -0300

    working buddy

--- Kernel/memory/buddyAlloc.c
+++ Kernel/memory/buddyAlloc.c
@@ -1,201 +1,222 @@
 // This is a personal academic project. Dear PVS-Studio, please check it.
 // PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
 #include "../include/memory_manager.h"
 
 #ifdef BUDDY
 
-#define PAGE_SIZE 4096 // (4KB)
 
-typedef struct block {
-    struct block *next;
-    size_t size;
-}block_t;
-
-static block_t** free_blocks;
-static uint8_t *base_memory;
-static size_t max_memory_size;
-static int max_order;
-
-int freeMem;
-int total;
-
-static int firstAllocRun = 1;
-static int firstFreeRun = 1;
-
-static int free_recursion_counter = 0;
-static int alloc_recursion_counter = 0;
+#define MIN_ALLOC 32//2^4
 
 
-
-/* blocks in freelists[i] are of size 2**(i + 2) * PAGE_SIZE */
-#define BLOCKSIZE(i) ((size_t)(1 << (i + 2)) * PAGE_SIZE)
-
-/* the address of the buddy of a block from freelists[i] */
-#define BUDDYOF(b, i) ((block_t*)((uintptr_t)(b) ^ (BLOCKSIZE(i))))
+#define BUDDY_START (unsigned)0xF00000
+//tamaño de 2^20
 
 
-int log2(size_t n) {
-    int log = 0;
-    while (n >>= 1)
-        log++;
-    return log;
+#define TOTALMEM 1048576 //2^20
+
+enum StateMem {
+    FULL, EMPTY, SPLIT
+};
+
+typedef struct node {
+    struct node *left;
+    struct node *right;
+    unsigned index;
+    void *memPtr;
+    unsigned size;
+    enum StateMem state;
+} node;
+
+#define IS_POWER_OF_2(x) (!((x)&((x)-1)))
+
+
+static node *root;
+unsigned memoryAllocated = 0;
+
+/* - Move to left child:     index = index * 2 + 1;
+* - Move to right child:    index = index * 2 + 2;*/
+
+
+
+static unsigned fixsize(unsigned size) {
+    size |= size >> 1;
+    size |= size >> 2;
+    size |= size >> 4;
+    size |= size >> 8;
+    size |= size >> 16;
+    return size + 1;
 }
 
-size_t pow2(int n) {
-    return (size_t)1 << n;
+
+void* createSons(node *parent) {
+    unsigned idx = parent->index * 2 + 1;
+    parent->left = parent + idx;
+
+    if ((uint64_t) parent->left >= BUDDY_START) {
+        return NULL;
+    }
+    parent->left->index = idx;
+    parent->left->size = parent->size / 2;
+    parent->left->memPtr = parent->memPtr;
+    parent->left->state = EMPTY;
+
+    uint64_t aux = (uint64_t)(parent->memPtr) + (parent->size / 2);
+
+
+    parent->right = parent + idx + 1;
+    if ((uint64_t) parent->right >= (uint64_t)BUDDY_START){
+        return NULL;
+    }
+    parent->right->index = idx + 1;
+    parent->right->size = parent->size / 2;
+
+    parent->right->memPtr = (void *) aux;
+    parent->right->state = EMPTY;
 }
 
-
-void mm_init(void * ptr, size_t max_size) {
-    base_memory = (uint8_t*) ptr;
-    freeMem = total = max_memory_size = max_size;
-
-    max_order = log2(max_size/PAGE_SIZE) - 2;
-
-    size_t free_blocks_size = (max_order + 1) * sizeof(block_t*);
-    free_blocks = (block_t**)base_memory;
-    base_memory += free_blocks_size;
-
-    uintptr_t addr = (uintptr_t)base_memory;
-    addr = (addr + PAGE_SIZE - 1) & ~(PAGE_SIZE - 1);
-    base_memory = (uint8_t *)addr;
-
-    for(int i= 0; i <= max_order; i++){
-        free_blocks[i] = NULL;
-    }
-
-    int initial_order = max_order;
-    size_t block_size = BLOCKSIZE(initial_order);
-
-    block_t * initial_block = (block_t *)base_memory;
-    free_blocks[initial_order] = initial_block;
-    initial_block->next = NULL;
-    initial_block->size = block_size;
-}
-
-void* mm_alloc(size_t size) {
-    int first_call = (alloc_recursion_counter == 0);
-    alloc_recursion_counter++;
-
-   if (first_call && firstAllocRun) {
-        freeMem -= size;
-        firstAllocRun = !firstAllocRun;
-    }
-    int i = 0;
-
-    while(BLOCKSIZE(i) < size + sizeof(block_t) && i <= max_order){
-        i++;
-    }
-
-    void* result = NULL;
-    if(i > max_order){
-         result = NULL;
-    } else if (free_blocks[i] != NULL) {
-        firstAllocRun = !firstAllocRun;
-        block_t* block = free_blocks[i];
-        free_blocks[i] = free_blocks[i]->next;
-        block->size = BLOCKSIZE(i);
-        result = (void*)(block);
-    } else {
-        block_t* block = (block_t*)mm_alloc(BLOCKSIZE(i + 1) - sizeof(block_t));
-        if (block != NULL) {
-            block = (block_t*)((uintptr_t)block - (uintptr_t)base_memory); // reative position
-
-            block_t* buddy = BUDDYOF(block, i);
-            buddy = (block_t*)((uintptr_t)buddy + (uintptr_t)base_memory); // reverse to absolute after getting the buddy
-
-            buddy->size = BLOCKSIZE(i);
-            buddy->next = free_blocks[i];
-
-            free_blocks[i] = buddy;
-            block = (block_t*)((uintptr_t)block + (uintptr_t)base_memory);  // reverse to absolute after getting the buddy
-            block->size = BLOCKSIZE(i);
-            result = (void*)(block);
-        }
-    }
-
-    alloc_recursion_counter--;
-
-    if (alloc_recursion_counter == 0) {
-        firstAllocRun = 1;
-    }
-
-    return result;
-}
-
-void mm_free(void * ptr) {
-    free_recursion_counter++;
-     if(firstFreeRun){
-        freeMem += ((block_t*)(ptr-sizeof(block_t)))->size;
-        firstAllocRun = !firstAllocRun;
-    }
-    block_t *block = (block_t *)ptr - sizeof(block_t) ;
-    int i = 0;
-    while(BLOCKSIZE(i) < block->size && i <= max_order){
-        i++;
-    }
-
-    if(i > max_order){
-        free_recursion_counter--;
-        // Resetear firstFreeRun si no hay más llamadas recursivas
-        if (free_recursion_counter == 0) {
-            firstFreeRun = 1;
-        }
+void stateUpdate(node *actual) {
+    if (actual->right == NULL || actual->left == NULL) {
+        actual->state = EMPTY;
         return;
     }
-    block = (block_t*)((uintptr_t)block - (uintptr_t)base_memory); // relativo
-    block_t* buddy = BUDDYOF(block, i);
-    block = (block_t*)((uintptr_t)block + (uintptr_t)base_memory);
-    buddy = (block_t*)((uintptr_t)buddy + (uintptr_t)base_memory);
-    block_t** p = &free_blocks[i];
+    if (actual->left->state == FULL && actual->right->state == FULL)
+        actual->state = FULL;
+    else if (actual->left->state == SPLIT || actual->right->state == SPLIT ||
+             actual->left->state == FULL || actual->right->state == FULL)
+        actual->state = SPLIT;
+    else
+        actual->state = EMPTY;
+}
 
-    //verifico que no se encuetre en la lisat de bloques libres
-    while ((*p != NULL) && (*p != buddy)) {
-        p = &(*p)->next;
+void *allocRec(node *actual, unsigned size) {
+    if (actual->state == FULL) {
+        return NULL;
     }
 
-    if(*p != buddy){
-        block->next = free_blocks[i];
-        free_blocks[i] = block;
-    }else {
-        // el buddy se encuntra libre, fusionamos los bloques
-        *p = buddy->next;
-
-        if(block > buddy){
-            mm_free(buddy + sizeof(block_t));
-        }else{
-            mm_free(block + sizeof(block_t));
+    if (actual->left != NULL || actual->right != NULL) { //si tiene hijos es porque este nodo ya esta particionado
+        void *aux = allocRec(actual->left, size);
+        if (aux == NULL) {
+            aux = allocRec(actual->right, size);
         }
+
+        stateUpdate(actual);
+
+        return aux;
+
+    } else {
+
+        if (size > actual->size) {
+            return NULL;
+        }
+
+        if (actual->size / 2 >= size) {
+            createSons(actual);
+            void *aux = allocRec(actual->left, size);
+            stateUpdate(actual);
+            return aux;
+
+        }
+        actual->state = FULL;
+        memoryAllocated += size;
+        return actual->memPtr;
     }
 
-    free_recursion_counter--;
-    // Resetear firstFreeRun si no hay más llamadas recursivas
-    if (free_recursion_counter == 0) {
-        firstFreeRun = 1;
-    }
 }
 
 
-void mm_state(){
+void *mm_alloc(size_t size) {
+    if (size > root->size) {
+        return NULL;
+    }
+
+    if (size < MIN_ALLOC)
+        size = MIN_ALLOC;
+
+    if (!IS_POWER_OF_2(size)) {
+        size = fixsize(size);
+    }
+
+    void *puntero = allocRec(root, size);
+    return puntero;
+}
+
+
+int freeRec(node *actual, void *block) {
+    if (actual->left != NULL || actual->right != NULL) {
+        int ret;
+        if (actual->right!=0 && (uint64_t) actual->right->memPtr > (uint64_t) block) {
+            ret = freeRec(actual->left, block);
+
+        } else {
+            ret = freeRec(actual->right, block);
+        }
+
+        stateUpdate(actual);
+
+        if (actual->state == EMPTY) {
+            actual->right = NULL;
+            actual->left = NULL;
+
+        }
+        return ret;
+
+
+    } else if (actual->state == FULL) {
+        if (actual->memPtr == block) {
+            actual->state = EMPTY;
+            memoryAllocated -= actual->size;
+            return 0;
+        }
+
+        return -1;
+
+    }
+
+
+    return -1;
+}
+
+
+void mm_free(void *block) {
+    int i = freeRec(root, block);
+    if (i == -1) {
+        return;
+    }
+}
+
+void mm_init(void * ptr, size_t max_size) {
+    root = (node *)ptr;
+    root->index = 0;
+    root->size = max_size;
+    root->state = EMPTY;
+    root->memPtr = (void *) BUDDY_START;
+}
+
+
+void mm_state() {
     char s[20];
-    int longitud;
+    unsigned longitud;
+    unsigned totalMem = TOTALMEM;
+    unsigned takenMem = memoryAllocated;
+    unsigned freeMem = totalMem - takenMem;
 
-    longitud = intToString(total, s);
-    write(1, "Memoria total: ", 15);
+    longitud = intToString(totalMem, s);
+    write(1, "Total memory: ", 15);
     write(1, s, longitud);
     write(1, "\n", 1);
 
 
-    longitud = intToString(freeMem, s);
-    write(1, "Memoria libre: ", 15);
-    write(1, s, longitud);
+    longitud = intToString(totalMem - takenMem, s);
+    write(1, "Free memory: ", 15);
+    write(1, s,  longitud);
     write(1, "\n", 1);
 
-    longitud = intToString(total - freeMem, s);
-    write(1, "Memoria ocupada: ", 17);
+    longitud = intToString(takenMem, s);
+    write(1, "Allocated memory: ", 17);
     write(1, s, longitud);
     write(1, "\n", 1);
-
 }
+
 
 #endif
